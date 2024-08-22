14161d4da9a6f7aca4f922a0e8f49e6647b8a78f
Rayhan Mahmud <rayhan.mahmuud@gmail.com>
2024-08-22 16:06:30 +0600

    configure the service app

--- mytube/settings.py
+++ mytube/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'service.apps.ServiceConfig',
 ]
 
 MIDDLEWARE = [
---
+++ service/urls.py
@@ -0,0 +1,6 @@
+
+app_name = 'service'
+
+urlpatterns = [
+    
+]
