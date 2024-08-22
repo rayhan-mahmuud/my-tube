33560f372fe612da5b5b20f2b8f5035fe28d8c88
Rayhan Mahmud <rayhan.mahmuud@gmail.com>
2024-08-22 18:49:10 +0600

    Create Folder and Video Models, make first migrations

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from service.models import Folder, Video
+
+admin.site.register(Folder)
+admin.site.register(Video)
--- service/models.py
+++ service/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from embed_video.fields import EmbedVideoField
 
-# Create your models here.
+
+class Folder(models.Model):
+    name = models.CharField(max_length=100)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='subfolders')
+    is_public = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return f"{self.name}"
+    
+
+class Video(models.Model):
+    name = models.CharField(max_length=255)
+    desc = models.CharField(max_length=255, null=True)
+    url = EmbedVideoField()
+    folder = models.ForeignKey(Folder, on_delete=models.CASCADE, related_name='videos')
+    added_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return f"{self.folder.name}/{self.name}"
+    
+    
+    
+
+    
+    
+
